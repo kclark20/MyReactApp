fddaf5bc251fd5165982196430e2ab2968943405
kclark20 <42810970+kclark20@users.noreply.github.com>
2018-08-29 16:27:51 -0500

    Update index.js
    Trying to build a tic-tac -toe game

--- app/index.js
+++ app/index.js
@@ -1,9 +1,26 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
-var App =
-require("./components/App");
+import React from ('react');
+import ReactDOM from ('react-dom');
+import App from ('./components/App');
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square value = {i} />;
+    }
+}
+
+class Square extends React.Component {
+    render(){
+        return(
+            <button className="square">
+            {this.props.value}
+            </button>
+        );
+    }
+
+}
+
 
 ReactDOM.render(
     <App />,
-    document.getElementById("app")
+    document.getElementById('app')
 );
